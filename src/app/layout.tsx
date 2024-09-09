4581f1493a588eb12d706ad094e0441f32875831
Ani-ket <dutta.aniket1399@gmail.com>
2024-09-09 16:10:34 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const amazonEmber = localFont({
+  src: "./fonts/Amazon Ember.ttf",
+  variable: "--font-amazon-ember",
+  weight: "400 700",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${amazonEmber.variable} antialiased`}>
         {children}
       </body>
     </html>
