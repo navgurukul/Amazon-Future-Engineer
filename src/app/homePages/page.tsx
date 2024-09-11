e0716831cfa9f0c907d3b13ab90c0b45d2dc7d26
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-09-11 18:47:16 +0530

    started home page

---
+++ src/app/homePages/page.tsx
@@ -0,0 +1,288 @@
+import type { NextPage } from "next";
+import { useCallback } from "react";
+import styles from "./_components/homePages.module.css";
+
+const HomePage: NextPage = () => {
+  const onFrameContainerClick = useCallback(() => {
+    // Add your code here
+  }, []);
+
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.frequentlyAskedQuestions}>
+        Frequently Asked Questions
+      </div>
+      <div className={styles.story}>
+        <div className={styles.fellowships}>Fellowships</div>
+        <div className={styles.faqQuestion}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What is the purpose of the Code India Fellowship?
+            </b>
+            <div className={styles.codeIndiaFellowship}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="remove_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild3} />
+      </div>
+      <div className={styles.videoParent}>
+        <div className={styles.video} />
+        <img
+          className={styles.reshotIconMoleculesYbnsd562}
+          alt=""
+          src="reshot-icon-molecules-YBNSD562JC 1.svg"
+        />
+        <div className={styles.frameParent}>
+          <div className={styles.welcomeToAmazonFutureEnginParent}>
+            <div className={styles.welcomeToAmazon}>
+              WELCOME TO AMAZON FUTURE ENGINEERS INNOVATION HUB
+            </div>
+            <div className={styles.aTinkeringLab}>
+              A tinkering lab for every future engineer
+            </div>
+          </div>
+          <div className={styles.frameGroup}>
+            <div
+              className={styles.bookASessionWrapper}
+            //   onClick={onFrameContainerClick}
+            >
+              <div className={styles.bookASession}>Book a Session</div>
+            </div>
+            <div className={styles.takeAVirtualTourWrapper}>
+              <div className={styles.bookASession}>Take a Virtual Tour</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.loginWrapper}
+        //  onClick={onFrameContainerClick}
+         >
+          <div className={styles.bookASession}>Login</div>
+        </div>
+        <div className={styles.frameContainer}>
+          <div className={styles.engWrapper}>
+            <div className={styles.eng}>Eng</div>
+          </div>
+          <div className={styles.wrapper}>
+            <div className={styles.eng}>ಅಇಈ</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameDiv}>
+        <div className={styles.frameParent1}>
+          <div className={styles.nanoSprintsParent}>
+            <div className={styles.welcomeToAmazon}>Nano Sprints</div>
+            <div className={styles.oneDaySessions}>
+              One day sessions designed to spark interest and build aspiration
+            </div>
+          </div>
+          <div className={styles.bookASessionContainer}>
+            <div className={styles.bookASession}>Book a Session</div>
+          </div>
+        </div>
+        <div className={styles.miniSprintsParent}>
+          <div className={styles.miniSprints}>Mini Sprints</div>
+          <div className={styles.codeIndiaFellowship}>
+            Multi-day workshops focusing on specific STEM topics
+          </div>
+          <div className={styles.knowMoreWrapper}>
+            <div className={styles.bookASession}>Know More</div>
+          </div>
+        </div>
+        <div className={styles.megaSprintsParent}>
+          <div className={styles.miniSprints}>Mega Sprints</div>
+          <div className={styles.codeIndiaFellowship}>
+            Multi-day workshops focusing on specific STEM topics
+          </div>
+          <div className={styles.knowMoreWrapper}>
+            <div className={styles.bookASession}>Know More</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.haveAnyQuestionsParent}>
+        <div className={styles.welcomeToAmazon}>Have Any Questions?</div>
+        <b className={styles.reachOutToContainer}>
+          <span>{`Reach out to us at `}</span>
+          <span className={styles.afeinnovationihubcom}>
+            afeinnovation@ihub.com
+          </span>
+        </b>
+      </div>
+      <div className={styles.frameParent2}>
+        <div className={styles.whatIsTheAfeInnovationHubParent}>
+          <div className={styles.innovationHub}>
+            What is the AFE Innovation Hub?
+          </div>
+          <div className={styles.afeStudioIs}>
+            AFE Studio is a state-of-the-art Robotics and AI lab dedicated to
+            empowering over 4,000 students by 2024. Our lab offers an
+            unparalleled opportunity for students to dive into the fascinating
+            realms of computer science and robotics. Designed with innovation in
+            mind, AFE Studio provides an interactive and immersive environment
+            where students can experiment, learn, and grow
+          </div>
+        </div>
+        <img className={styles.frameChild} alt="" src="Rectangle 1.png" />
+        <div className={styles.rectangleParent}>
+          <img className={styles.frameItem} alt="" src="Rectangle 4.svg" />
+          <img className={styles.frameInner} alt="" src="Rectangle 3.svg" />
+          <img className={styles.frameItem} alt="" src="Rectangle 5.svg" />
+        </div>
+      </div>
+      <div className={styles.latestFromTheInnovationHubParent}>
+        <div className={styles.welcomeToAmazon}>
+          Latest From The Innovation Hub
+        </div>
+        <img className={styles.frameChild} alt="" src="Rectangle 1.png" />
+        <div className={styles.frameWrapper}>
+          <div className={styles.chevronLeftParent}>
+            <img
+              className={styles.chevronLeftIcon}
+              alt=""
+              src="chevron_left.svg"
+            />
+            <img className={styles.frameChild2} alt="" src="Rectangle 4.svg" />
+            <img className={styles.frameChild2} alt="" src="Rectangle 5.svg" />
+            <img className={styles.frameChild4} alt="" src="Rectangle 3.svg" />
+            <img
+              className={styles.chevronLeftIcon}
+              alt=""
+              src="chevron_right.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
