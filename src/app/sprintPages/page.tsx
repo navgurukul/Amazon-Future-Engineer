11e152589a64764fa83c028cdd671a69b243b18e
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-09-17 12:24:44 +0530

    started mini sprint page

---
+++ src/app/sprintPages/page.tsx
@@ -0,0 +1,382 @@
+import styles from "./_components/miniSprint.module.css";
+import type { NextPage } from "next";
+import { useCallback } from "react";
+
+const MiniSprints: NextPage = () => {
+  const onFrameContainerClick = useCallback(() => {
+    // Add your code here
+  }, []);
+
+  return (
+    <div className={styles.miniSprints}>
+      <div className={styles.frequentlyAskedQuestions}>
+        Frequently Asked Questions
+      </div>
+      <div className={styles.story}>
+        <div className={styles.fellowships}>Fellowships</div>
+        <div className={styles.faqQuestion}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What is the purpose of the Code India Fellowship?
+            </b>
+            <div className={styles.codeIndiaFellowship}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="remove_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild} />
+        <div className={styles.faqQuestion1}>
+          <div className={styles.question}>
+            <b className={styles.whatIsThe}>
+              What will a Code India Fellow do?
+            </b>
+            <div className={styles.codeIndiaFellowship1}>
+              Code India Fellowship aims to transform the learning and life
+              outcomes of young women/girl students studying in government high
+              schools by placing fellows. CIF will enable young women to have
+              jobs or admission to advanced training opportunities by the time
+              they finish their school education.
+            </div>
+          </div>
+          <img
+            className={styles.removeCircleOutlineIcon}
+            alt=""
+            src="add_circle_outline.svg"
+          />
+        </div>
+        <div className={styles.storyChild3} />
+      </div>
+      <div className={styles.multiDaySkillBuildingWorks}>
+        Multi-Day Skill-Building Workshops on specific STEM Topics
+      </div>
+      <div className={styles.whyIsMini}>
+        Why is Mini Sprint a Great Fit for Your Class?
+      </div>
+      <div className={styles.frameParent}>
+        <div className={styles.vectorParent}>
+          <img className={styles.vectorIcon} alt="" src="Vector.png" />
+          <div className={styles.nanoSprints}>Nano Sprints</div>
+        </div>
+        <div className={styles.vectorGroup}>
+          <img className={styles.vectorIcon1} alt="" src="Vector.png" />
+          <div className={styles.innovationHub}>Mini Sprints</div>
+        </div>
+        <div className={styles.vectorContainer} 
+        // onClick={onFrameContainerClick}
+        >
+          <img className={styles.vectorIcon} alt="" src="Vector.png" />
+          <div className={styles.nanoSprints}>Mega Sprints</div>
+        </div>
+      </div>
+      <div className={styles.frameGroup}>
+        <div className={styles.frameContainer}>
+          <div className={styles.nanoSprintsParent}>
+            <div className={styles.whyScheduleA}>Nano Sprints</div>
+            <div className={styles.oneDaySessions}>
+              One day sessions designed to spark interest and build aspiration
+            </div>
+          </div>
+          <div className={styles.bookASessionWrapper}>
+            <div className={styles.bookASession}>Book a Session</div>
+          </div>
+        </div>
+        <div className={styles.miniSprintsParent}>
+          <div className={styles.miniSprints2}>Mini Sprints</div>
+          <div className={styles.codeIndiaFellowship}>
+            Multi-day workshops focusing on specific STEM topics
+          </div>
+          <div className={styles.knowMoreWrapper}>
+            <div className={styles.bookASession}>Know More</div>
+          </div>
+        </div>
+        <div className={styles.megaSprintsParent}>
+          <div className={styles.miniSprints2}>Mega Sprints</div>
+          <div className={styles.codeIndiaFellowship}>
+            Multi-day workshops focusing on specific STEM topics
+          </div>
+          <div className={styles.knowMoreWrapper}>
+            <div className={styles.bookASession}>Know More</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rectangleParent}>
+        <img className={styles.frameChild} alt="" src="Rectangle 4.png" />
+        <img className={styles.frameChild} alt="" src="Rectangle 3.png" />
+      </div>
+      <div className={styles.rectangleGroup}>
+        <img className={styles.frameChild} alt="" src="Rectangle 4.png" />
+        <img className={styles.frameChild} alt="" src="Rectangle 3.png" />
+        <img className={styles.frameChild} alt="" src="Rectangle 5.png" />
+      </div>
+      <div className={styles.frameDiv}>
+        <div className={styles.reshotIconMoleculesYbnsd562Parent}>
+          <img
+            className={styles.reshotIconMoleculesYbnsd562}
+            alt=""
+            src="reshot-icon-molecules-YBNSD562JC 1.svg"
+          />
+          <div className={styles.innovationHub}>Innovation Hub</div>
+        </div>
+        <b className={styles.privacyPolicy}>Privacy Policy</b>
+        <div className={styles.linkedinParent}>
+          <img className={styles.linkedinIcon} alt="" src="Linkedin.svg" />
+          <img className={styles.linkedinIcon} alt="" src="Twitter.svg" />
+        </div>
+      </div>
+      <div className={styles.homeSprintsContainer}>
+        <span className={styles.home}>
+          <b className={styles.home1}>Home</b>
+        </span>
+        <span className={styles.sprints}>
+          <span className={styles.home}>{` / `}</span>
+          <span className={styles.sprints1}>Sprints</span>
+        </span>
+      </div>
+      <div className={styles.whyScheduleAMiniSprintParent}>
+        <div className={styles.whyScheduleA}>Why Schedule a Mini Sprint? </div>
+        <div className={styles.miniSprintsOffer}>
+          Mini Sprints offer a rich, multi-day experience that allows students
+          to develop and strengthen their skills in key STEM areas. With
+          hands-on projects and expert guidance, this program is ideal for
+          schools and teachers looking to provide students with a more in-depth
+          and engaging learning experience. 
+        </div>
+      </div>
+      <div className={styles.readyToElevateStudentSkillParent}>
+        <div className={styles.whyScheduleA}>
+          Ready to Elevate Student Skills? 
+        </div>
+        <div className={styles.miniSprintsOffer}>
+          Book a Mini Sprint today and provide your students with the
+          opportunity to dive deeper into the world of technology. Contact us
+          now to schedule your session and embark on an enriching STEM
+          adventure!
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.ellipseParent}>
+          <div className={styles.ellipseDiv} />
+          <div className={styles.rectangleDiv} />
+          <div className={styles.ellipseDiv} />
+        </div>
+        <div className={styles.polygonParent}>
+          <img className={styles.polygonIcon} alt="" src="Polygon 1.svg" />
+          <div className={styles.firstVisitParent}>
+            <b className={styles.firstVisit}>First Visit</b>
+            <div className={styles.codeIndiaFellowship}>
+              Dive deeper into specific STEM topics with focused, multi-day
+              workshops. 
+            </div>
+          </div>
+        </div>
+        <div className={styles.polygonGroup}>
+          <img className={styles.frameChild3} alt="" src="Polygon 1.svg" />
+          <div className={styles.firstVisitParent}>
+            <b className={styles.firstVisit}>Hands-On Learning</b>
+            <div className={styles.codeIndiaFellowship}>
+              Build and refine essential skills with practical, hands-on
+              activities and tools
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.miniSprintsInner}>
+        <div className={styles.frameParent1}>
+          <div className={styles.reshotIconSearchTimeScheduParent}>
+            <div className={styles.reshotIconSearchTimeSchedu}>
+              <div className={styles.group}>
+                <img className={styles.groupIcon} alt="" src="Group.svg" />
+                <img className={styles.groupIcon1} alt="" src="Group.svg" />
+                <img className={styles.groupIcon2} alt="" src="Group.svg" />
+                <img className={styles.groupIcon3} alt="" src="Group.svg" />
+                <img className={styles.groupIcon4} alt="" src="Group.svg" />
+                <div className={styles.group1}>
+                  <div className={styles.group2}>
+                    <img className={styles.groupIcon5} alt="" src="Group.svg" />
+                    <img className={styles.groupIcon6} alt="" src="Group.svg" />
+                  </div>
+                  <img className={styles.groupIcon7} alt="" src="Group.svg" />
+                  <div className={styles.group3}>
+                    <img className={styles.groupIcon8} alt="" src="Group.svg" />
+                    <img className={styles.groupIcon9} alt="" src="Group.svg" />
+                  </div>
+                  <img className={styles.groupIcon10} alt="" src="Group.svg" />
+                </div>
+                <img className={styles.groupIcon11} alt="" src="Group.svg" />
+                <img className={styles.groupIcon12} alt="" src="Group.svg" />
+                <img className={styles.groupIcon13} alt="" src="Group.svg" />
+                <img className={styles.groupIcon14} alt="" src="Group.svg" />
+                <img className={styles.groupIcon15} alt="" src="Group.svg" />
+              </div>
+            </div>
+            <div className={styles.durationParent}>
+              <b className={styles.whatIsThe}>Duration</b>
+              <div className={styles.bookASession}>
+                3 Days (2 hours per day) 
+              </div>
+            </div>
+          </div>
+          <div className={styles.reshotIconSearchTimeScheduParent}>
+            <img
+              className={styles.reshotIconSearchTimeSchedu}
+              alt=""
+              src="reshot-icon-student-boy-L9ESXQZ3WU.svg"
+            />
+            <div className={styles.durationParent}>
+              <b className={styles.whatIsThe}>Batch Strength</b>
+              <div className={styles.bookASession}>
+                20 to 30 students per session 
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameWrapper}>
+            <div
+              className={styles.joinWaitingListWrapper}
+            //   onClick={onFrameContainerClick}
+            >
+              <div className={styles.bookASession}>Join Waiting List</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.haveAnyQuestionsParent}>
+        <div className={styles.whyScheduleA}>Have Any Questions?</div>
+        <b className={styles.reachOutToContainer}>
+          <span>{`Reach out to us at `}</span>
+          <span className={styles.afeinnovationihubcom}>
+            afeinnovation@ihub.com
+          </span>
+        </b>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.frameParent2}>
+          <div className={styles.frameParent3}>
+            <div className={styles.engWrapper}>
+              <div className={styles.eng}>Eng</div>
+            </div>
+            <div className={styles.wrapper}>
+              <div className={styles.eng}>ಅಇಈ</div>
+            </div>
+          </div>
+          <img className={styles.ellipseIcon} alt="" src="Ellipse 1.png" />
+        </div>
+        <img
+          className={styles.reshotIconMoleculesYbnsd5621}
+          alt=""
+          src="reshot-icon-molecules-YBNSD562JC 1.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MiniSprints;
