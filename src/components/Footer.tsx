ffafcd4e7159d013997a8e43e1f7c12392c364a3
Tamannatanwa <tamanna22@navgurukul.org>
2024-10-09 01:45:07 +0530

    footer & login worked on design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
-"use client"
-import React from "react";
+"use client";
+
+import { Button } from "./ui/button";
 import type { NextPage } from "next";
 import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
 // import router from "next/router";
 
 interface FooterProps {
@@ -11,47 +14,48 @@ interface FooterProps {
 
 const Footer: NextPage<FooterProps> = ({ handleOfflineBooking }) => {
   const router = useRouter();
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const onReshotIconClick = () => {
     router.push("/");
   };
 
+  const handleResize = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const whatsappLink = `https://wa.me/${6366969292}`;
+
   return (
-    <div className="w-full bg-[#ffad33] h-auto text-center text-lg text-gray-800 font-amazon-ember py-4">
-      <div className="flex flex-col md:flex-row justify-between items-center h-full px-6">
+    <div className="w-full bg-orange-main h-auto text-center text-lg text-gray-800 font-amazon-ember  px-4 py-12 md:py-8 md:px-12 shadow-lg">
+      <div className="flex flex-col md:flex-row justify-between items-center h-full gap-6 md:gap-0">
         {/* Logo Section */}
-        <div className="flex flex-row items-center gap-4 text-xl font-amazon-ember-display">
-          <div className="hidden md:flex items-center gap-[5px]">
-            <Image
-              className="object-contain cursor-pointer mt-2"
-              alt="Reshot Icon"
-              src="/login/Group (2).svg"
-              onClick={onReshotIconClick}
-              width={64}
-              height={64}
-            />
+        <div className="">
+          <div className="hidden md:flex">
             <Image
               className="object-contain cursor-pointer"
               alt="Reshot Icon"
-              src="/login/Group (3).svg"
+              src="/login/afe_subbrand_logo_horizontal_blue.svg"
               onClick={onReshotIconClick}
-              width={150}
-              height={70}
+              width={254}
+              height={40}
             />
           </div>
           <div className="md:hidden">
-          <Image
-              className="object-contain cursor-pointer mt-2"
-              alt="Reshot Icon"
-              src="/login/Group (2).svg"
-              onClick={onReshotIconClick}
-              width={34}
-              height={34}
-            />
-          <Image
+            <Image
               className="object-contain cursor-pointer"
               alt="Reshot Icon"
-              src="/login/Group (3).svg"
+              src="/login/Group(12).svg"
               onClick={onReshotIconClick}
               width={120}
               height={40}
@@ -60,23 +64,46 @@ const Footer: NextPage<FooterProps> = ({ handleOfflineBooking }) => {
         </div>
 
         {/* Privacy Policy */}
-        <div className="mt-4 md:mt-0 font-medium leading-[170%] order-2 md:order-none">
+        <div className="flex justify-center w-full md:w-auto text-body1 font-body1-regular text-darkslategray">
           Privacy Policy
         </div>
 
         {/* Helpdesk Section */}
-        <div
-          className="flex flex-row items-center justify-center gap-2 px-6 py-2 mt-4 md:mt-0 rounded-full bg-gray-200 text-orange-600 order-3 md:order-none cursor-pointer"
-          onClick={handleOfflineBooking}
-        >
-          <Image
-            className="w-8 h-8 object-cover"
-            alt="Helpdesk Icon"
-            src="/nanopage/reshot-icon-friendly-customer-service-C63QKLHVB9.svg"
-            width={354}
-            height={40}
-          />
-          <div className="font-medium leading-[170%]">Call Us</div>
+        {/* Helpdesk Section */}
+        <div className="flex gap-4 w-full md:w-auto">
+          <Button
+            variant="proceedWhite"
+            onClick={handleOfflineBooking}
+            className="flex-grow flex justify-center items-center gap-3 px-4 py-2"
+          >
+            <Image
+              alt="Helpdesk Icon"
+              src="/nanopage/reshot-icon-friendly-customer-service-C63QKLHVB9.svg"
+              width={24}
+              height={24}
+            />
+            <span className="relative font-medium leading-[170%] text-base">
+              Call Us
+            </span>
+          </Button>
+
+          {isMobile && (
+            <Button
+              variant="proceedWhite"
+              onClick={handleOfflineBooking}
+              className="flex-grow flex justify-center items-center gap-3 px-4 py-2"
+            >
+              <Image
+                alt="WhatsApp Icon"
+                src="/login/reshot-icon-whatsapp-UANBKF398R 1.svg"
+                width={24}
+                height={24}
+              />
+              <span className="relative font-medium leading-[170%] text-base text-darkslategray">
+                Chat with Us
+              </span>
+            </Button>
+          )}
         </div>
       </div>
     </div>
@@ -84,4 +111,3 @@ const Footer: NextPage<FooterProps> = ({ handleOfflineBooking }) => {
 };
 
 export default Footer;
-
