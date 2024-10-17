16f721625c8aaa08c5d66351fc8da1fc2553da2b
Ani-ket <dutta.aniket1399@gmail.com>
2024-10-17 17:27:52 +0530

    popup changes

--- src/app/admin/dashboard/calendar/_components/Calendar.tsx
+++ src/app/admin/dashboard/calendar/_components/Calendar.tsx
@@ -34,28 +34,28 @@ const Calendar: React.FC = () => {
         <div>
           {/* <div className="pt-[120px] px-4 md:px-8 pb-[48px]"> */}
           <div className="mt-8 px-4 md:px-8 pb-[48px]">
-<div className="w-full relative h-14 flex flex-row items-center justify-between text-center text-lg text-white font-['Amazon Ember'] mb-8">
-  <Button className="bg-[#f55c38] rounded-full h-14 flex items-center justify-center px-8 py-2 box-border">
-    <span className="relative font-medium leading-[170%]">Create Booking</span>
-  </Button>
-  <Button className="bg-[#f091b2] rounded-full h-14 flex items-center justify-center px-8 py-2 box-border gap-2">
-    <Image width={24} height={24} className="w-6 h-6 relative flex-shrink-0" alt="settings" src={settingsIcon} />
-    <span onClick={handleManageSlotsClick} className="relative font-medium leading-[170%]">Manage Slots</span>
-  </Button>
-</div>
-{showTimeSlotCalendar ? ( // Conditionally render TimeSlotCalendar based on state
+            <div className="w-full relative h-14 flex flex-row items-center justify-between text-center text-lg text-white font-['Amazon Ember'] mb-8">
+              <Button className="bg-[#f55c38] rounded-full h-14 flex items-center justify-center px-8 py-2 box-border">
+                <span className="relative font-medium leading-[170%]">Create Booking</span>
+              </Button>
+              <Button className="bg-[#f091b2] rounded-full h-14 flex items-center justify-center px-8 py-2 box-border gap-2">
+                <Image width={24} height={24} className="w-6 h-6 relative flex-shrink-0" alt="settings" src={settingsIcon} />
+                <span onClick={handleManageSlotsClick} className="relative font-medium leading-[170%]">Manage Slots</span>
+              </Button>
+            </div>
+            {showTimeSlotCalendar ? ( // Conditionally render TimeSlotCalendar based on state
               <TimeSlotCalendar />
             ) : (
-            <div className="flex flex-col lg:flex-row w-full justify-center">
-              <div className="w-full lg:w-[100%]">
-                {/* <FullCalendarComponent setSelectedDate={setSelectedDate} /> */}
-                <TimeSlotCalendar/>
+              <div className="flex flex-col lg:flex-row w-full justify-center">
+                <div className="w-full lg:w-[100%]">
+                  <FullCalendarComponent setSelectedDate={setSelectedDate} />
+                  {/* <TimeSlotCalendar /> */}
+                </div>
+                <div className="block lg:hidden py-6">
+                  <hr className="border-gray-300 my-4" />
+                </div>
               </div>
-              <div className="block lg:hidden py-6">
-                <hr className="border-gray-300 my-4" />
-              </div>
-            </div>
-          )}
+            )}
           </div>
         </div>
       )}
